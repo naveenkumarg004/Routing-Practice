3cdabd7e5768bf5f72c3d39189bfd1fbf5fb0de6
naveenkumarg004 <gnaveenkumar166@gmail.com>
2023-05-02 10:35:24 +0000

    first commit

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-cont">
+    <img
+      className="not-found-image"
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      alt="not found"
+    />
+    <h1 className="para">Not Found</h1>
+  </div>
+)
+
+export default NotFound
